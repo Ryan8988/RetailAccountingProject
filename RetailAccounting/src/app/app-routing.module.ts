f178504486176983a0f27bf00617b1c5b2b52e2d
Yuanzheng Zhang <zhangyz89@gmail.com>
2018-11-08 13:34:45 -0500

    1.build router path 2.get data from firebase 3.build angular material sale list

--- RetailAccounting/src/app/app-routing.module.ts
+++ RetailAccounting/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SaleListComponent} from './sale-list/sale-list.component';
+import {InventoryComponent} from './inventory/inventory.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: SaleListComponent},
+  {path: 'inventory', component: InventoryComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- RetailAccounting/src/app/app.module.ts
+++ RetailAccounting/src/app/app.module.ts
@@ -3,24 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
 import { SaleListComponent } from './sale-list/sale-list.component';
 import { InventoryComponent } from './inventory/inventory.component';
 import {HttpClientModule} from '@angular/common/http';
 import {SaleListService} from './sale-list/sale-list.service';
 import {DataStotageService} from './shared/data-stotage.service';
+import {MatTableModule} from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
+    NavigationComponent,
     SaleListComponent,
     InventoryComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatTableModule,
+    FlexLayoutModule
   ],
   providers: [SaleListService, DataStotageService],
   bootstrap: [AppComponent]
